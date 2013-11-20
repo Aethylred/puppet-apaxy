3407bfd2bc8195ddb9564b6341bfda5a4fe286d9
Aaron Hicks <hicksa@landcareresearch.co.nz>
2013-11-20 15:31:43 +1300

    Manage the header.html and footer.html files in the theme.

--- manifests/theme.pp
+++ manifests/theme.pp
@@ -1,16 +1,20 @@
 define apaxy::theme (
-  $docroot      = $apaxy::params::docroot,
-  $directory    = undef,
-  $manage_vhost = undef
+  $docroot        = $apaxy::params::docroot,
+  $header_source  = undef,
+  $footer_source  = undef,
+  $directory      = undef,
+  $manage_vhost   = undef
 ){
   require apaxy
 
   # docroot in name to ensure resources are unique
 
   file{"${docroot}_theme":
-    ensure  => link,
+    ensure  => directory,
     path    => "${docroot}/theme",
-    target  => $apaxy::theme_dir,
+    recurse => true,
+    replace => false,
+    source  => $apaxy::theme_dir,
     require => File['apaxy_theme_dir'],
   }
 
@@ -20,6 +24,38 @@ define apaxy::theme (
     content => template('apaxy/htaccess.erb'),
     require => File["${docroot}_theme"],
     notify  => Service['httpd'],
+    }
+
+  if $header_source {
+    file{"${docroot}_header":
+      ensure  => file,
+      path    => "${docroot}/theme/header.html",
+      source  => $header_source,
+      require => File["${docroot}_theme"],
+    }
+  } else {
+    file{"${docroot}_header":
+      ensure  => file,
+      path    => "${docroot}/theme/header.html",
+      content => template('apaxy/header.html.erb'),
+      require => File["${docroot}_theme"],
+    }
+  }
+
+  if $footer_source {
+    file{"${docroot}_footer":
+      ensure  => file,
+      path    => "${docroot}/theme/footer.html",
+      source  => $footer_source,
+      require => File["${docroot}_theme"],
+    }
+  } else {
+    file{"${docroot}_footer":
+      ensure  => file,
+      path    => "${docroot}/theme/footer.html",
+      content => template('apaxy/footer.html.erb'),
+      require => File["${docroot}_theme"],
+    }
   }
 
   if $manage_vhost {
